f2ec4ef6a19ce66087e0389c6b27d6673adcaba4
IamIisI <titusj915@gmail.com>
2025-01-09 16:39:41 -0800

    Add method structure to SmartDashboardUtils

--- src/main/java/frc/robot/utils/SmartDashboardUtils.java
+++ src/main/java/frc/robot/utils/SmartDashboardUtils.java
@@ -4,5 +4,49 @@
 
 package frc.robot.utils;
 
-/** Add your docs here. */
-public class SmartDashboardUtils {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.utils.Elastic.ElasticNotification;
+
+/** Where all SmartDashbard vales will be edited */
+public class SmartDashboardUtils extends SubsystemBase{
+
+    private DriveSubsystem driveSubsystem;
+
+    /**
+     * Constructor for SmartDashboardUtils
+     * 
+     * @param robotContainer
+     */
+    public SmartDashboardUtils(DriveSubsystem driveSubsystem) {
+        this.driveSubsystem = driveSubsystem;
+        dashboardInit();
+    }
+
+    /**
+     * Initializes the values for the SmartDashboard and gets all required instances
+     */
+    public void dashboardInit() {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates the values on the SmartDashboard
+     */
+    @Override
+    public void periodic() {
+       try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendElasticNotification(ElasticNotification notification){
+        Elastic.sendAlert(notification);
+    }
+}
