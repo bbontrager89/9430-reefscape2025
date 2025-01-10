a3b0feb0443bbd814a6f51a49ee0059ef34d41f3
IamIisI <titusj915@gmail.com>
2025-01-09 19:38:39 -0800

    Format code
    also add an example for double pressing a d pad button twice with a > 250ms delay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,8 +13,10 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -35,164 +37,187 @@ import java.util.List;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+    // The robot's subsystems
+    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-  // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+    // The driver's controller
+    XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-  // The driver's controller
-  XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
+    // The driver's controller
+    XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
 
+    Double povUpPressedRecently = null;
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the button bindings
+        configureButtonBindings();
 
-    // Configure default commands
-    m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                true),
-            m_robotDrive));
-  }
+        // Configure default commands
+        m_robotDrive.setDefaultCommand(
+                // The left stick controls translation of the robot.
+                // Turning is controlled by the X axis of the right stick.
+                new RunCommand(
+                        () -> m_robotDrive.drive(
+                                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                                true),
+                        m_robotDrive));
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
-   * subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
-   * passing it to a
-   * {@link JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Right Trigger - Coral manipulator wheels intake
-    new JoystickButton(m_driverController, edu.wpi.first.wpilibj.PS4Controller.Button.kR1.value)
-        /*.whileTrue(new RunCommand())*/;
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+     * subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+     * passing it to a
+     * {@link JoystickButton}.
+     */
+    private void configureButtonBindings() {
 
-    // Left Trigger - Coral manipulator wheels out
-    new JoystickButton(m_driverController, edu.wpi.first.wpilibj.PS4Controller.Button.kL1.value)
-        /*.whileTrue(new RunCommand())*/;
-    
-    // Y button - Coral mode
-    new JoystickButton(m_operatorController, Button.kY.value)
-    .onTrue((new InstantCommand()));
+        // Right Trigger - Coral manipulator wheels intake
+        new JoystickButton(m_driverController, edu.wpi.first.wpilibj.PS4Controller.Button.kR1.value)
+        /* .whileTrue(new RunCommand()) */;
 
-    // X button - Algae clear from reef mode
-    new JoystickButton(m_operatorController, Button.kX.value)
-    .onTrue((new InstantCommand()));
+        // Left Trigger - Coral manipulator wheels out
+        new JoystickButton(m_driverController, edu.wpi.first.wpilibj.PS4Controller.Button.kL1.value)
+        /* .whileTrue(new RunCommand()) */;
 
-    // B button - Algae intake mode
-    new JoystickButton(m_operatorController, Button.kB.value)
-    .onTrue((new InstantCommand()));
+        // Y button - Coral mode
+        new JoystickButton(m_operatorController, Button.kY.value)
+                .onTrue((new InstantCommand()));
 
-    // A button - Algae intake mode
-    new JoystickButton(m_operatorController, Button.kA.value)
-    .onTrue((new InstantCommand()));
+        // X button - Algae clear from reef mode
+        new JoystickButton(m_operatorController, Button.kX.value)
+                .onTrue((new InstantCommand()));
 
-    // Right Stick button - Transit mode
-    new JoystickButton(m_operatorController, Button.kRightStick.value)
-    .onTrue((new InstantCommand()));
+        // B button - Algae intake mode
+        new JoystickButton(m_operatorController, Button.kB.value)
+                .onTrue((new InstantCommand()));
 
-    // Left Stick button - unbound?
-    new JoystickButton(m_operatorController, Button.kLeftStick.value)
-    .onTrue((new InstantCommand()));
+        // A button - Algae intake mode
+        new JoystickButton(m_operatorController, Button.kA.value)
+                .onTrue((new InstantCommand()));
 
-    // Dpad Up button
-    new POVButton(m_operatorController, 0)
-    .onTrue((new InstantCommand()));
+        // Right Stick button - Transit mode
+        new JoystickButton(m_operatorController, Button.kRightStick.value)
+                .onTrue((new InstantCommand()));
 
-    // Dpad Up-Right button
-    new POVButton(m_operatorController, 45)
-    .onTrue((new InstantCommand()));
+        // Left Stick button - unbound?
+        new JoystickButton(m_operatorController, Button.kLeftStick.value)
+                .onTrue((new InstantCommand()));
 
-    // Dpad Right button
-    new POVButton(m_operatorController, 90)
-    .onTrue((new InstantCommand()));
+        // Dpad Up button
+        new POVButton(m_driverController, 0)
+                .onTrue((new InstantCommand(new Runnable() {
 
-    // Dpad Down-Right button
-    new POVButton(m_operatorController, 135)
-    .onTrue((new InstantCommand()));
+                    @Override
+                    public void run() {
+                        if (povUpPressedRecently != null) {
+                            if (povUpPressedRecently + 0.25 > Timer.getFPGATimestamp()) {
+                                System.out.println("Dpad Up button pressed twice");
+                                SmartDashboard.putBoolean("pressedTwice", true);
+                                SmartDashboard.putBoolean("pressedOnce", false);
+                            } else {
+                                System.out.println("Dpad pressed once");
+                                povUpPressedRecently = Timer.getFPGATimestamp();
+                                SmartDashboard.putBoolean("pressedTwice", false);
+                                SmartDashboard.putBoolean("pressedOnce", true);
+                            }
+                        } else {
+                            System.out.println("Dpad pressed first");
+                            SmartDashboard.putBoolean("pressedOnce", true);
+                            povUpPressedRecently = Timer.getFPGATimestamp();
+                        }
+                    }
 
-    // Dpad Down button
-    new POVButton(m_operatorController, 180)
-    .onTrue((new InstantCommand()));
+                })));
 
-    // Dpad Down-Left button
-    new POVButton(m_operatorController, 225)
-    .onTrue((new InstantCommand()));
+        // Dpad Up-Right button
+        new POVButton(m_operatorController, 45)
+                .onTrue((new InstantCommand()));
 
-    // Dpad Left button
-    new POVButton(m_operatorController, 270)
-    .onTrue((new InstantCommand()));
+        // Dpad Right button
+        new POVButton(m_operatorController, 90)
+                .onTrue((new InstantCommand()));
 
-    // Dpad Up-Left button
-    new POVButton(m_operatorController, 315)
-    .onTrue((new InstantCommand()));
+        // Dpad Down-Right button
+        new POVButton(m_operatorController, 135)
+                .onTrue((new InstantCommand()));
 
-    // Start Button button - Manual mode
-    new JoystickButton(m_operatorController, Button.kStart.value)
-    .onTrue((new InstantCommand()));
+        // Dpad Down button
+        new POVButton(m_operatorController, 180)
+                .onTrue((new InstantCommand()));
 
-    // Back Button button - 
-    new JoystickButton(m_operatorController, Button.kBack.value)
-    .onTrue((new InstantCommand()));
+        // Dpad Down-Left button
+        new POVButton(m_operatorController, 225)
+                .onTrue((new InstantCommand()));
 
-    
-  }
+        // Dpad Left button
+        new POVButton(m_operatorController, 270)
+                .onTrue((new InstantCommand()));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(DriveConstants.kDriveKinematics);
+        // Dpad Up-Left button
+        new POVButton(m_operatorController, 315)
+                .onTrue((new InstantCommand()));
 
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        config);
+        // Start Button button - Manual mode
+        new JoystickButton(m_operatorController, Button.kStart.value)
+                .onTrue((new InstantCommand()));
 
-    var thetaController = new ProfiledPIDController(
-        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        // Back Button button -
+        new JoystickButton(m_operatorController, Button.kBack.value)
+                .onTrue((new InstantCommand()));
 
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        exampleTrajectory,
-        m_robotDrive::getPose, // Functional interface to feed supplier
-        DriveConstants.kDriveKinematics,
+    }
 
-        // Position controllers
-        new PIDController(AutoConstants.kPXController, 0, 0),
-        new PIDController(AutoConstants.kPYController, 0, 0),
-        thetaController,
-        m_robotDrive::setModuleStates,
-        m_robotDrive);
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // Create config for trajectory
+        TrajectoryConfig config = new TrajectoryConfig(
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                // Add kinematics to ensure max speed is actually obeyed
+                .setKinematics(DriveConstants.kDriveKinematics);
 
-    // Reset odometry to the starting pose of the trajectory.
-    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+        // An example trajectory to follow. All units in meters.
+        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+                // Start at the origin facing the +X direction
+                new Pose2d(0, 0, new Rotation2d(0)),
+                // Pass through these two interior waypoints, making an 's' curve path
+                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                // End 3 meters straight ahead of where we started, facing forward
+                new Pose2d(3, 0, new Rotation2d(0)),
+                config);
 
-    // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
-  }
+        var thetaController = new ProfiledPIDController(
+                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+                exampleTrajectory,
+                m_robotDrive::getPose, // Functional interface to feed supplier
+                DriveConstants.kDriveKinematics,
+
+                // Position controllers
+                new PIDController(AutoConstants.kPXController, 0, 0),
+                new PIDController(AutoConstants.kPYController, 0, 0),
+                thetaController,
+                m_robotDrive::setModuleStates,
+                m_robotDrive);
+
+        // Reset odometry to the starting pose of the trajectory.
+        m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+        // Run path following command, then stop at the end.
+        return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
+    }
 }
