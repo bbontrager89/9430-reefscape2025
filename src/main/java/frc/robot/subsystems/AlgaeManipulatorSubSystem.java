2cb0be398eeae13f0e5cfbb154978813ce7434fc
Dabz26 <dylan.dablow@gmail.com>
2025-01-13 18:48:57 -0800

    Create AlgaeManipulatorSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/AlgaeManipulatorSubSystem.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class AlgaeManipulatorSubSystem extends SubsystemBase {
+
+  private SparkFlex rightAlgaeManipulatorMotor = new SparkFlex(Constants.RightAlgaeManipulatorCANid, MotorType.kBrushless);
+  private SparkFlex leftAlgaeManipulatorMotor = new SparkFlex(Constants.LeftAlgaeManipulatorCANid, MotorType.kBrushless);
+
+
+  /** Creates a new AlgaeManipulatorSubSystem. */
+  public AlgaeManipulatorSubSystem() {
+    
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
