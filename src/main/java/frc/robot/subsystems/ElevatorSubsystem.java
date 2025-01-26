6805d9885ea97d867deb924704717a87a0cf9c43
IamIisI <titusj915@gmail.com>
2025-01-25 17:43:02 -0800

    Add command chooser for scoring positions

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -12,7 +12,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SoftLimitConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
@@ -30,6 +33,8 @@ public class ElevatorSubsystem extends SubsystemBase {
   private boolean aboveMaxHeight = false;
   private boolean belowMinHeight = false;
   private double currentSpeed = 0.0;
+
+  private SendableChooser<Command> elevatorCommands;
 
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
@@ -52,6 +57,43 @@ public class ElevatorSubsystem extends SubsystemBase {
     elevatorMotorConfig.apply(softLimitConfig);
 
     elevatorMotor.configure(elevatorMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+
+    elevatorCommands = new SendableChooser<Command>();
+
+    elevatorCommands.addOption("SP 1", new InstantCommand(new Runnable() {
+      @Override
+      public void run() {
+        moveToScoringPosition(1);
+      }
+    }));
+    elevatorCommands.addOption("SP 2", new InstantCommand(new Runnable() {
+      @Override
+      public void run() {
+        moveToScoringPosition(2);
+      }
+    }));
+    elevatorCommands.addOption("SP 3", new InstantCommand(new Runnable() {
+      @Override
+      public void run() {
+        moveToScoringPosition(3);
+      }
+    }));
+    elevatorCommands.addOption("SP 4", new InstantCommand(new Runnable() {
+      @Override
+      public void run() {
+        moveToScoringPosition(4);
+      }
+    }));
+    elevatorCommands.addOption("SP 5", new InstantCommand(new Runnable() {
+      @Override
+      public void run() {
+        moveToScoringPosition(5);
+      }
+    }));
+
+    SmartDashboard.putData("Elevator Commands", elevatorCommands);
+
+    SmartDashboard.putBoolean("Run Elevator Command", false);
 
   }
 
@@ -90,8 +132,17 @@ public class ElevatorSubsystem extends SubsystemBase {
         break;
     }
 
+    SmartDashboard.putNumber("desiredHeight", desiredHeight);
+
     // ;;;;;;;;desiredHeight=(scoringPosition==1)?ElevatorConstants.level1ScoringPosition:(scoringPosition==2)?ElevatorConstants.level3ScoringPosition:(scoringPosition==3)?ElevatorConstants.level3ScoringPosition/*IWouldNotRecommend-Titus(ButItIsFunny)*/:(scoringPosition==4)?ElevatorConstants.level4ScoringPosition:-1.0;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+    autoMode = true;
+
+  }
+
+  public void moveToPosition(double position) {
+
+    desiredHeight = position;
     autoMode = true;
 
   }
@@ -161,6 +212,12 @@ public class ElevatorSubsystem extends SubsystemBase {
         setMotorSpeed(autoSpeed);
       }
 
+    }
+
+    // Dashboard Commands
+    if (SmartDashboard.getBoolean("Run Elevator Command", false)) {
+      SmartDashboard.putBoolean("Run Elevator Command", false);
+      elevatorCommands.getSelected().schedule();
     }
 
   }
