b59c0408ebbe598a69b88c0d89ca82cbed049a6d
IamIisI <titusj915@gmail.com>
2025-02-18 19:27:14 -0800

    Always return true in MoveElevator

--- src/main/java/frc/robot/commands/MoveElevator.java
+++ src/main/java/frc/robot/commands/MoveElevator.java
@@ -41,6 +41,6 @@ public class MoveElevator extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return elevator.atHeight();
+    return (elevator.atHeight() || true);
   }
 }
