5bc6443e395783704dedff9a86f6f93b2c029d9e
IamIisI <titusj915@gmail.com>
2025-02-18 17:09:53 -0800

    change TransitModeCommand to a sequential command group

--- src/main/java/frc/robot/commands/TransitModeCommand.java
+++ src/main/java/frc/robot/commands/TransitModeCommand.java
@@ -4,44 +4,22 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.CoralManipulatorConstants;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.CoralManipulatorSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TransitModeCommand extends Command {
-
-  ElevatorSubsystem elevator;
-  CoralManipulatorSubsystem coral;
+public class TransitModeCommand extends SequentialCommandGroup {
 
   /** Creates a new TransitModeCommand. */
   public TransitModeCommand(ElevatorSubsystem elevator, CoralManipulatorSubsystem coral) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(elevator, coral);
 
-    this.elevator = elevator;
-    this.coral = coral;
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    elevator.moveToScoringPosition(0);
-    coral.movePivotTo(CoralManipulatorConstants.maximumPivotPosition);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return elevator.atHeight() && coral.atPivotPosition();
+    addCommands(
+      new SequentialCommandGroup(
+        // new PivotAlgae(algae, 0)
+        new MoveElevator(elevator, 4), 
+        new PivotCoral(coral, 0)));
   }
 }
