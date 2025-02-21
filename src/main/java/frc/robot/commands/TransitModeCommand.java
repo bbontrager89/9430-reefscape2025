d75a398a4b9e1a2915ce90ff0d3721d8503cb14d
IamIisI <titusj915@gmail.com>
2025-02-20 18:12:02 -0800

    Remove Unused Import

--- src/main/java/frc/robot/commands/TransitModeCommand.java
+++ src/main/java/frc/robot/commands/TransitModeCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.CoralManipulatorConstants;
 import frc.robot.subsystems.CoralManipulatorSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
