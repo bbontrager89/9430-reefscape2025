6f24bb0eb4af6f1cb830b837ecc07217ec50b9cc
IamIisI <titusj915@gmail.com>
2025-03-02 13:31:12 -0800

    Remove Unused imports and definitions

--- src/main/java/frc/robot/subsystems/ClimbingArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbingArmSubsystem.java
@@ -4,13 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkFlexConfig;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbingArmConstants;
 
@@ -19,15 +14,8 @@ public class ClimbingArmSubsystem extends SubsystemBase {
   private SparkFlex motor1 = new SparkFlex(ClimbingArmConstants.motor1CanId, MotorType.kBrushless);
   private SparkFlex motor2 = new SparkFlex(ClimbingArmConstants.motor2CanId, MotorType.kBrushless);
 
-  private SparkFlexConfig motorConfig;
-
   /** Creates a new ClimbingArmSubsystem. */
-  public ClimbingArmSubsystem() {
-    motorConfig = new SparkFlexConfig();
-
-    motor1.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    motor2.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+  public ClimbingArmSubsystem() {}
 
   /**
    * Sets the speed of the motors controlling the climbing arm
