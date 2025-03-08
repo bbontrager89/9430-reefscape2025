befc31346e3c99ebfe7244a233bdaa0e6149e156
IamIisI <titusj915@gmail.com>
2025-03-07 19:29:07 -0800

    Tune constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -106,8 +106,8 @@ public final class Constants {
     public static final double scoringDistanceRight = 0.58;
     public static final double scoringDistanceLeft = 0.48;
 
-    public static final double rightCoralIntakeDistance = 0.53;
-    public static final double leftCoralIntakeDistance = 0.47;
+    public static final double rightCoralIntakeDistance = 0.40;
+    public static final double leftCoralIntakeDistance = 0.40;
 
     public static final double leftScoringOffset = -0.161;
     public static final double rightScoringOffset = 0.165;
@@ -152,7 +152,7 @@ public final class Constants {
     public static final double minimumElevatorHeight = (newRobot)? 0.165 : 0.175; // Furthest possible is 0.164
     public static final double maximumElevatorHeight = (newRobot)? 0.975 : 0.675; // Furthest possible is 0.99
 
-    public static final double coralStationPosition = (newRobot)? 0.495 : 0.405; // Scoring position 0
+    public static final double coralStationPosition = (newRobot)? 0.496 : 0.405; // Scoring position 0
     public static final double level1ScoringPosition = (newRobot)? 0.411 : 0.32;
     public static final double level2ScoringPosition = (newRobot)? 0.511 : 0.51;
     public static final double level3ScoringPosition = (newRobot)? 0.777 : 0.669;
